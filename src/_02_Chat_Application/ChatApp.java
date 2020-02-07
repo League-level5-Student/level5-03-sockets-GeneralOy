1769126eab73da8bc69060a7bdf5c7f4da7ae3c6
League Student <league@iMac-20.attlocal.net>
2020-02-06 16:59:26 -0800

    work on the formatting of the frame

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -1,9 +1,48 @@
 package _02_Chat_Application;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 /*
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
 public class ChatApp {
-
+	JPanel mainPanel = new JPanel();
+	JFrame mainFrame = new JFrame();
+	JButton sendMessage = new JButton();
+	JTextField textIn = new JTextField();
+	JScrollPane mainPane = new JScrollPane();
+	JTextArea textArea = new JTextArea();
+	JLabel clientNum = new JLabel();
+	
+	Server server;
+	Client client;
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp() {
+		
+		mainPanel.add(mainPane);
+		mainFrame.add(mainPanel);
+		
+		mainPane.add(textArea);
+		mainPanel.add(clientNum);
+		mainPanel.add(textIn);
+		mainPanel.add(sendMessage);
+		mainFrame.setSize(200, 400);
+		mainFrame.setVisible(true);
+		textIn.setSize(200, 50);
+	}
+	
+	
+	
+	
+	
 }
